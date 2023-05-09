65953000c04bc6ed0b73926e740b1293dad393ae
Dominik Gresch <greschd@users.noreply.github.com>
2023-05-09 09:17:23 +0200

    Adapt to ansys org rename (#25)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ if __name__ == "__main__":
         description=description,
         long_description=long_description,
         long_description_content_type='text/markdown',
-        url=f"https://github.com/ansys/{package_name}",
+        url=f"https://github.com/ansys-internal/{package_name}",
         license="MIT",
         python_requires=">=3.7",
         install_requires=["grpcio~=1.17", "protobuf~=3.19"],
