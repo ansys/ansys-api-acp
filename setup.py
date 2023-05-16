c3f79fa9a2d890e4121d8952c91cc35a3e8fd4d4
Dominik Gresch <greschd@users.noreply.github.com>
2023-05-16 13:55:25 +0200

    Relax protobuf requirement (#26)
    Analogous to
    https://github.com/ansys-internal/ansys-api-template/pull/13

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ if __name__ == "__main__":
         url=f"https://github.com/ansys-internal/{package_name}",
         license="MIT",
         python_requires=">=3.7",
-        install_requires=["grpcio~=1.17", "protobuf~=3.19"],
+        install_requires=["grpcio~=1.17", "protobuf>=3.19,<5"],
         package_dir = {"": "src"},
         packages=setuptools.find_namespace_packages("src", include=("ansys.*",)),
         package_data={
